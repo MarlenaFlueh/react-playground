ece68aaff97144955036e620127746c2991f1ab0
m.flueh <mf@esyoil.com>
2018-03-03 18:07:53 +0100

    add bind and arrow function

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
     ]
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState(
       {
         persons: [
-          { name: "Liam", age: 29 },
+          { name: newName, age: 29 },
           { name: "Marlena", age: 21 },
           { name: "Tim", age: 22 }
         ]
@@ -27,10 +27,17 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, i'm a react app</h1>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I'm cool!</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-        <button onClick={this.switchNameHandler}>Switch me</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age} />
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}>I'm cool!</Person>
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}
+        click={() => this.switchNameHandler('Maxi')} />
+        <button onClick={this.switchNameHandler.bind(this, 'Maxi')}>Switch me</button>
       </div>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'hi there!'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
   return (
     <div>
-      <p>{props.name} is {props.age} years old.</p>
+      <p onClick={props.click}>{props.name} is {props.age} years old.</p>
       <p>{props.children}</p>
     </div>
   )
