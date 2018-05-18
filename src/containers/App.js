dcde45a1522de7ca428f19cff3a994cb0d35bf51
MarlenaFlueh <mf@esyoil.com>
2018-05-18 17:46:01 +0200

    add another hoc component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,13 @@
-import React, {PureComponent} from 'react';
-import styles from './App.css';
-import Persons from '../components/Persons/Persons';
-import Cockpit from '../components/Cockpit/Cockpit';
-
+import React, { PureComponent } from "react";
+import styles from "./App.css";
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
+import WithClass from "../hoc/WithClass";
 
 class App extends PureComponent {
   constructor(props) {
     super(props);
-    console.log('[App.js] Inside constructor.', props);
+    console.log("[App.js] Inside constructor.", props);
     this.state = {
       persons: [
         { id: 1, name: "Liam", age: 22 },
@@ -15,15 +15,15 @@ class App extends PureComponent {
         { id: 3, name: "Tom", age: 22 }
       ],
       showPersons: false
-    }
-  };
+    };
+  }
 
   componentWillMount() {
-    console.log('[App.js] Inside componentWillMount.')
+    console.log("[App.js] Inside componentWillMount.");
   }
 
   componentDidMount() {
-    console.log('[App.js] Inside componentDidMount.');
+    console.log("[App.js] Inside componentDidMount.");
   }
 
   // shouldComponentUpdate(nextProps, nextState) {
@@ -33,62 +33,75 @@ class App extends PureComponent {
   // }
 
   componentWillUpdate(nextProps, nextState) {
-    console.log('[Update App.js] Inside componentWillUpdate', nextProps, nextState);
+    console.log(
+      "[Update App.js] Inside componentWillUpdate",
+      nextProps,
+      nextState
+    );
   }
 
   componentDidUpdate() {
-    console.log('[Update App.js] Inside componentDidUpdate');
+    console.log("[Update App.js] Inside componentDidUpdate");
   }
 
   nameChangeHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex((p) => {
+    const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
-    })
+    });
 
-    
-      const person = {
-        ...this.state.persons[personIndex]
-      };
+    const person = {
+      ...this.state.persons[personIndex]
+    };
 
-      person.name = event.target.value;
+    person.name = event.target.value;
 
-      const persons = [...this.state.persons];
-      persons[personIndex] = person;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
 
-      this.setState({persons});
+    this.setState({ persons });
   };
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState({ showPersons: !doesShow });
   };
 
-  deletePersonHandler = (personsIndex) => {
+  deletePersonHandler = personsIndex => {
     const persons = [...this.state.persons];
     persons.splice(personsIndex, 1);
-    this.setState({persons});
+    this.setState({ persons });
   };
 
   render() {
-    console.log('[App.js] Inside render.');
+    console.log("[App.js] Inside render.");
     let persons = null;
 
     if (this.state.showPersons) {
-      persons = <Persons
-        persons={this.state.persons}
-        click={this.deletePersonHandler}
-        changed={this.nameChangeHandler} />
+      persons = (
+        <Persons
+          persons={this.state.persons}
+          click={this.deletePersonHandler}
+          changed={this.nameChangeHandler}
+        />
+      );
     }
 
     return (
-        <div className={styles.App}>
-          <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
-          <Cockpit
+      <WithClass styles={styles.App}>
+        <button
+          onClick={() => {
+            this.setState({ showPersons: true });
+          }}
+        >
+          Show Persons
+        </button>
+        <Cockpit
           showPersons={this.state.showPersons}
           persons={this.state.persons}
-          click={this.togglePersonsHandler} />
-          {persons}
-        </div>
+          click={this.togglePersonsHandler}
+        />
+        {persons}
+      </WithClass>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'hi there!'));
   }
