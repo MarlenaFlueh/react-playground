8d7ecd5a71f63dac31a7833f95e6acef6a113111
MarlenaFlueh <mf@esyoil.com>
2018-05-18 18:43:38 +0200

    using prevState

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,8 @@ class App extends PureComponent {
         { id: 2, name: "Marlena", age: 21 },
         { id: 3, name: "Tom", age: 22 }
       ],
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0
     };
   }
 
@@ -64,7 +65,12 @@ class App extends PureComponent {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
+    this.setState((prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      };
+    });
   };
 
   deletePersonHandler = personsIndex => {
