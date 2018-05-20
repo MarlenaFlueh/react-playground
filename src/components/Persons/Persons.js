c30a0a33e22debddf485771138fe497b1f755947
MarlenaFlueh <mf@esyoil.com>
2018-05-20 19:18:36 +0200

    add ref for input

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,22 +1,25 @@
-import React, {PureComponent} from 'react';
-import Person from './Person/Person';
+import React, { PureComponent } from "react";
+import Person from "./Person/Person";
 
 class Persons extends PureComponent {
   constructor(props) {
     super(props);
-    console.log('[Persons.js] Inside constructor.', props);
-  };
+    console.log("[Persons.js] Inside constructor.", props);
+  }
 
   componentWillMount() {
-    console.log('[Persons.js] Inside componentWillMount.')
+    console.log("[Persons.js] Inside componentWillMount.");
   }
 
   componentDidMount() {
-    console.log('[Persons.js] Inside componentDidMount.');
+    console.log("[Persons.js] Inside componentDidMount.");
   }
 
   componentWillReceiveProps(nextProps) {
-    console.log('[Update Person.js] Inside componentWillReseiveProps', nextProps);
+    console.log(
+      "[Update Person.js] Inside componentWillReseiveProps",
+      nextProps
+    );
   }
 
   // shouldComponentUpdate(nextProps, nextState) {
@@ -27,24 +30,32 @@ class Persons extends PureComponent {
   // }
 
   componentWillUpdate(nextProps, nextState) {
-    console.log('[Update Person.js] Inside componentWillUpdate', nextProps, nextState);
+    console.log(
+      "[Update Person.js] Inside componentWillUpdate",
+      nextProps,
+      nextState
+    );
   }
 
   componentDidUpdate() {
-    console.log('[Update Person.js] Inside componentDidUpdate');
+    console.log("[Update Person.js] Inside componentDidUpdate");
   }
-  
-  render () {
-    console.log('[Persons.js] Inside render.');
+
+  render() {
+    console.log("[Persons.js] Inside render.");
     return this.props.persons.map((person, index) => {
-      return <Person
-        click={() => this.props.click(index)}
-        name={person.name} 
-        age={person.age}
-        key={person.id}
-        changed={(event) => this.props.changed(event, person.id)} />
+      return (
+        <Person
+          position={index}
+          click={() => this.props.click(index)}
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={event => this.props.changed(event, person.id)}
+        />
+      );
     });
   }
 }
 
-  export default Persons;
+export default Persons;
